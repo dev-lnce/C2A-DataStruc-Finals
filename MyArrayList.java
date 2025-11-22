81cfbbc6481502f732314eefcdba7a9a9882cc56
darl-12 <darldimaculangan@gmail.com>
2025-11-22 20:07:52 +0800

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,66 +1,108 @@
 package Try;
 
 public class MyArrayList {
-    private int[] data;
+    private int[] array;
     private int size;
 
     public MyArrayList() {
-        data = new int[10];
+        array = new int[10];
         size = 0;
     }
 
-    private void grow() {
-        if (size == data.length) {
-            int[] biggerArray = new int[data.length * 2];
-            for (int i = 0; i < data.length; i++) {
-                biggerArray[i] = data[i];
+    private void makeBigger() {
+        if (size == array.length) {
+            int[] temp = new int[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                temp[i] = data[i];
             }
-            data = biggerArray;
+            array = temp;
         }
     }
 
-    public void addEnd(int value) {
-        grow();
-        data[size] = value;
+    public static void add(int num) {
+        makeBigger();
+        array[size] = num;
         size++;
+        showAll();
     }
 
-    public void addStart(int value) {
-        grow();
+   public static void addAtStart(int num) {
+        makeBigger();
         for (int i = size; i > 0; i--) {
-            data[i] = data[i - 1];
+            array[i] = array[i - 1];
         }
-        data[0] = value;
+        array[0] = num;
         size++;
+        showAll();
     }
 
-    public void addAtIndex(int index, int value) {
-        if (index < 0 || index > size) {
+    public static void insertAt(int position, int num2) {
+        if (position < 0 || position > size) {
+            System.out.println("Invalid position.");
             return;
         }
-        grow();
-        for (int i = size; i > index; i--) {
-            data[i] = data[i - 1];
+
+        makeBigger();
+
+        for (int i = size; i > position; i--) {
+            array[i] = array[i - 1];
         }
-        data[index] = value;
+
+        array[position] = num2;
         size++;
+        showAll();
     }
 
-    public String listAll() {
-        if (size == 0) {
-            return "Array List: [] (Empty)";
+    public static void removeByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Invalid Index");
+            return;
         }
-        String output = "Array List: [";
+
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        size--;
+        showAll();
+    }
+
+    public static void get(int index2) {
+        if (index2 < 0 || index2 >= size) {
+            System.out.println("Invalid index");
+            return;
+        }
+        System.out.println("Number at index " + index2 + ": " + array[index2]);
+    }
+
+    public static void getSize() {
+        System.out.println("Size: " + size);
+    }
+
+    public static void search(int num3) {
+        boolean found = false;
+
         for (int i = 0; i < size; i++) {
-            output += data[i];
-            if (i < size - 1) {
-                output += ", ";
+            if (array[i] == num3) {
+                System.out.println("Found at index " + i);
+                found = true;
             }
         }
-        output += "]";
-        return output;
-    }
 
+        if (!found) {
+            System.out.println("Number not found.");
+        }
+
+        showAll();
+    }
+    
+     public static void showAll() {
+        System.out.print("Items: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
     public boolean search(int value) {
         for (int i = 0; i < size; i++) {
             if (data[i] == value) {
@@ -70,19 +112,24 @@ public class MyArrayList {
         return false;
     }
 
-    public void sort() {
+    public static void sort() {
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
-                if (data[j] > data[j + 1]) {
-                    int temp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = temp;
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
+        System.out.println("Array sorted:");
+        showAll();
     }
 
-    public void clear() {
+    public static void clear() {
         size = 0;
+        System.out.println("All items are now cleared.");
     }
+
+
 }
