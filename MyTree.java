0b02cf5e13fae93b4fd6232849fed8dda6fdb1ca
Macky0007 <markguce05@gmail.com>
2025-11-22 22:33:17 +0800

    Update MyTree.java

--- MyTree.java
+++ MyTree.java
@@ -1,88 +1,187 @@
-package Try;
+import java.util.Scanner;
 
 public class MyTree {
+    BinaryTree btree= new BinaryTree();
+    Scanner sc = new Scanner(System.in);
+    String choice2;
+    public void run(){
+        System.out.println("====================================================");
+        System.out.println("          Welcome to Trees Implementation!          ");
+        System.out.println("====================================================");
+        do {
+            System.out.println("\nPick a Command You'd like to try: ");
+            System.out.println("1. Insert");
+            System.out.println("2. Delete");
+            System.out.println("3. Search");
+            System.out.println("4. Display");
+            System.out.println("5. Exit Trees");
+            System.out.print("Choice: ");
+            int choice1 = sc.nextInt();
 
-    private class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
+            switch (choice1) {
+                case 1:
+                    System.out.print("Enter value to add: ");
+                    int add = sc.nextInt();
+                    btree.insert(new Node(add));
+                    break;
+                case 2:
+                    System.out.print("Enter value to Delete: ");
+                    int delete = sc.nextInt();
+                    btree.remove(delete);
+                    break;
+                case 3:
+                    System.out.print("Enter value to Search: ");
+                    int search = sc.nextInt();
+                    boolean found = btree.search(search);
+                    if (found) {
+                        System.out.println(search + " was found in the tree.");
+                    } else {
+                        System.out.println(search + " was not found in the tree.");
+                    }
+                    break;
+                case 4:
+                    btree.display();
+                    break;
+                case 5:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+            System.out.print("Would you like to choose another option? (Y/N): ");
+            sc.nextLine();  // Clear the buffer from previous nextInt()
+            choice2 = sc.nextLine().trim();
         }
+        while(choice2.equalsIgnoreCase("Y"));
+    }
+}
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+class BinaryTree{
+    Node root;
+
+    public void insert(Node node) {
+        root = insertHelper(root, node);
     }
 
-    private Node root;
+    private Node insertHelper(Node root, Node node) {
+        int data = node.data;
 
-    public MyTree() {
-        root = null;
+        if(root==null){
+            root=node;
+            return root;
+        }
+        else if(data < root.data){
+            root.left=insertHelper(root.left, node);
+        }
+        else{
+            root.right=insertHelper(root.right, node);
+        }
+        return root;
     }
 
-    public void add(int value) {
-        root = addRecursive(root, value);
-    }
-
-    private Node addRecursive(Node currentNode, int value) {
-        if (currentNode == null) {
-            return new Node(value);
-        }
-
-        if (value < currentNode.data) {
-            currentNode.left = addRecursive(currentNode.left, value);
-        } else if (value > currentNode.data) {
-            currentNode.right = addRecursive(currentNode.right, value);
-        } else {
-        }
-        return currentNode;
-    }
-
-    public boolean search(int value) {
-        return searchRecursive(root, value);
-    }
-
-    private boolean searchRecursive(Node currentNode, int value) {
-        if (currentNode == null) {
-            return false;
-        }
-        if (value == currentNode.data) {
-            return true;
-        }
-        if (value < currentNode.data) {
-            return searchRecursive(currentNode.left, value);
-        } else {
-            return searchRecursive(currentNode.right, value);
-        }
-    }
-
-    public String listAll() {
+    public void display() {
         if (root == null) {
-            return "Tree: [] (Empty)";
+            System.out.println("Tree: [] (Empty)");
+            return;
         }
-        String output = "Tree (In-Order): [";
 
         StringBuilder sb = new StringBuilder();
-        listInOrder(root, sb);
+        displayHelper(root, sb);
 
         String result = sb.toString();
         if (result.length() > 2) {
             result = result.substring(0, result.length() - 2);
         }
 
-        return "Tree (In-Order): [" + result + "]";
+        System.out.println("Tree (In-Order): [" + result + "]");
     }
 
-    private void listInOrder(Node node, StringBuilder sb) {
+    private void displayHelper(Node node, StringBuilder sb) {
         if (node != null) {
-            listInOrder(node.left, sb);
+            displayHelper(node.left, sb);
             sb.append(node.data);
             sb.append(", ");
-            listInOrder(node.right, sb);
+            displayHelper(node.right, sb);
         }
     }
 
-    public void clear() {
-        root = null;
+    public boolean search(int data) {
+        return searchHelper(root, data);
+    }
+
+    private boolean searchHelper(Node root, int data) {
+        if(root==null){
+            return false;
+        }
+        else if(root.data==data){
+            return true;
+        }
+        else if(root.data>data){
+            return searchHelper(root.left, data);
+        }
+        else{
+            return searchHelper(root.right, data);
+        }
+    }
+
+    public void remove(int data) {
+        if(search(data)){
+            removeHelper(root, data);
+        }
+        else{
+            System.out.println(data + " Could not be found");
+        }
+    }
+
+    public Node removeHelper(Node root, int data) {
+        if(root==null){
+            return root;
+        }
+        else if(data < root.data){
+            root.left=removeHelper(root.left, data);
+        }
+        else if(data > root.data){
+            root.right=removeHelper(root.right, data);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                root = null;
+            }
+            else if(root.right!=null){ //find a successor to replace this node
+                root.data = successor(root);
+                root.right = removeHelper(root.right, root.data);
+            }
+            else{ //find a predecessor to replace this node
+                root.data = predecessor(root);
+                root.left = removeHelper(root.left, root.data);
+            }
+        }
+        return null;
+    }
+
+    private int successor(Node root){ //Find least value below the right child of this root node
+        root = root.right;
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root.data;
+    }
+
+    private int predecessor(Node root){ //Find greatest value below the left child of this root node
+        root = root.left;
+        while(root.right!=null){
+            root = root.right;
+        }
+        return root.data;
     }
 }
